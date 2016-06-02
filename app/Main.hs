546dbcdd9b73b53b9e92a9889ff8d22c544372c2
Echo Nolan <echo@echonolan.net>
2016-06-02 15:45:13 -0700

    For network PR

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 import Network.HTTP.Conduit
-import qualified Data.ByteString.Lazy.Char8 as LC8
+import System.IO
 
 main :: IO ()
 main = do
+    mapM_ (flip hSetBuffering LineBuffering) [stdin, stdout]
     man <- newManager tlsManagerSettings
     req <- parseUrl "https://s3.amazonaws.com/hackage.fpcomplete.com/package/lens-4.12.3.tar.gz"
-    response <- httpLbs req man
-    LC8.putStrLn $ responseBody response
+    _response <- httpLbs req man
+    pure ()
